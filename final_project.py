b427ec4a71042df37ea2850fc241217a4c03537c
csimner24 <csimner@uchicago.edu>
2024-12-09 17:10:42 -0600

    Done with page 1
    moving onto page 2, running the program (section)

--- final_project.py
+++ final_project.py
@@ -2,110 +2,44 @@
 import argparse
 import pickle
 from datetime import datetime
+import pickle
+import task_module as t
 
-class Task:
-    """Representation of a task
-  
-    Attributes:
-              - created - date
-              - completed - date
-              - name - string
-              - unique id - number
-              - priority - int value of 1, 2, or 3; 1 is default
-              - due date - date, this is optional and defaults to None
-    """
-    _new_id = 0
-    def __init__(self, name, priority= 1, due_date= None, completed= None):
-        self.created = self.__create_date()
-        self.completed = self.__validate_completed(completed) #This sets the default to None since the user won't set it right away, it performs error handling if there is a input passed
-        self.name = name
-        self.name = self.__validate_name(name)
-        self.unique_id = self.__create_unique_id()
-        self.priority = self.__validate_priority(priority)
-        self.due_date = self.__validate_due_date(due_date) #This sets the optional value of due date to None and performs error handling when a value is passed
-
-    def __create_date(self):
-        """This is a private method used to create the date value for the 'created' attribute. It returns a string."""
-        current = datetime.now()
-
-        formatted_date = current.strftime("%m/%d/%Y")
-
-        return formatted_date
-    
-    def __create_unique_id(self):
-        """This is a private method used to create the unique ID attribute for a Task. It returns an integer."""
-        Task._new_id += 1
-        return Task._new_id
-    
-    def __validate_date(self, date_value):
-        """This is a private method to validate if a date string is in MM/DD/YYYY format."""
-        try:
-            datetime.strptime(date_value, "%m/%d/%Y")
-            return True
-        except:
-            return False
-        
-    def __validate_name(self, name):
-        """This is a private method used to validate the name attribute or set the default value to None. It returns either a string name or None."""
-        name_check = name.replace(" ", "").strip()
-        if len(name_check) == 0:
-            print("You cannot enter a blank name, the name value will therefore be set to None.")
-            return None
-        else:
-            return name.strip()
-        
-    def __validate_completed(self, completed=None):
-        """This is a private method used to validate the completed attribute or set the default value to None. It returns either a formatted date or None."""
-        if completed is None:
-            return None
-        else:
-            if self.__validate_date(completed):
-                return completed
-            else:
-                print(f"You did not enter a correct date in MM/DD/YYYY format.") #When an error occurs, defaut the value back to None and display a message
-                return None 
-            
-    def __validate_due_date(self, due_date=None):
-        """This is a private method used to validate the completed attribute or set the default value to None. It returns either a formatted date or None."""
-        if due_date is None:
-            return None
-        else:
-            if self.__validate_date(due_date):
-                return due_date
-            else:
-                print("You did not enter a correct date in MM/DD/YYYY format.")
-                return None #When an error occurs, defaut the value back to None and display a message
-    
-    def __validate_priority(self, priority):
-        """This is a private method used to validate the priority attribute or set the default value to 1. It returns the integers 1, 2, or 3."""
-        if priority == 1:
-            return 1
-        elif priority == 2:
-            return 2
-        elif priority == 3:
-            return 3
-        else:
-            print("Please enter priority as an integer of either 1, 2, or 3. The default is 1.")
-            return 1
-
-
-
-
-
-
+cole = t.Task("cole", priority=3)
 
 
 
 class Tasks:
     """A list of 'Task' objects."""
-    def __init__(self):
+    def __init__(self, pickle_file=".todo.pickle"):
         """Read pickled tasks file into a list."""
         self.tasks = []
-        #code here
+        self.pickle_file = pickle_file
+        self.tasks = self.pickle_read()
+    
+    def pickle_read(self):
+        """Read from the pickle file to getr a list of tasks."""
+        try:
+            # Try to open the pickle file in read-binary mode
+            with open(self.pickle_file, "rb") as file:
+                # Load the list of tasks from the pickle file
+                self.tasks = pickle.load(file)
+        except FileNotFoundError:
+            print(f"The task list does not exist, the list of tasks is empty.")
+            return self.tasks
+        except Exception as e:
+            print(f"Error while loading tasks: {e}")
+            return self.tasks
 
     def pickle_tasks(self):
         """Pickle your task list to a file"""
-        #code here
+        try:
+            # Open the pickle file in write-binary mode
+            with open(self.pickle_file, "wb") as file:
+                pickle.dump(self.tasks, file)
+        except Exception as e:
+            # Handle any exception that occurs during saving
+            print(f"Error while saving tasks: {e}")
     
     ### add, list, report, done, delete, etc. functionality ###
 
@@ -133,4 +67,9 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    cole = Tasks()
+    print(cole)
+    print(cole.tasks)
+    steve = cole.tasks
+    jeff = len(steve)
+    print(jeff)
